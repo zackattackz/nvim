78e83b832898f82581314ab9fc37e234ab62d186
Zachary Hanham <z@zmhanham.com>
2024-12-23 16:04:18 -0500

    telescope: grep args + smart history

--- init.lua
+++ init.lua
@@ -5,9 +5,11 @@ vim.api.nvim_set_option_value("termguicolors", true, {})
 vim.o.relativenumber = true
 vim.o.number = true
 vim.o.statuscolumn = "%s %l %r"
+local telescope = require("telescope")
 local ts_actions = require("telescope.actions")
-local tsfb_actions = require("telescope").extensions.file_browser.actions
-require("telescope").setup({
+local tsfb_actions = telescope.extensions.file_browser.actions
+local ts_history_db_path = vim.fn.stdpath("data") .. "/databases/telescope_history.sqlite3"
+telescope.setup({
     defaults = {
         mappings = {
             i = {
@@ -15,8 +17,14 @@ require("telescope").setup({
                 ["<C-k>"] = ts_actions.move_selection_previous,
                 ["<C-j>"] = ts_actions.move_selection_next,
                 ["<C-l>"] = ts_actions.select_default,
+                ["<C-Down>"] = ts_actions.cycle_history_next,
+                ["<C-Up>"] = ts_actions.cycle_history_prev,
             },
         },
+        history = {
+            path = ts_history_db_path,
+            limit = 100,
+        }
     },
     extensions = {
         file_browser = {
@@ -29,9 +37,11 @@ require("telescope").setup({
         },
     },
 })
-require("telescope").load_extension('file_browser')
-require("telescope").load_extension('project')
-require("telescope").load_extension('ui-select')
+telescope.load_extension('smart_history')
+telescope.load_extension('file_browser')
+telescope.load_extension('project')
+telescope.load_extension('ui-select')
+telescope.load_extension("live_grep_args")
 require("hop").setup({})
 require("dapui").setup()
 require("trouble").setup()
@@ -90,6 +100,15 @@ function ToggleChadTree()
         end
     else
         vim.cmd("CHADopen")
+    end
+end
+
+function TouchTelescopeHistoryDB()
+    if vim.fn.filereadable(ts_history_db_path) == 0 then
+        local file = io.open(ts_history_db_path, "w")
+        if file then
+            file:close()
+        end
     end
 end
 
@@ -297,15 +316,15 @@ do
     local keybindings = {
         { action = "<cmd>Telescope find_files<cr>", key = "<leader> ", mode = "n" },
         { action = function () require('telescope.builtin').buffers({ sort_mru = true; ignore_current_buffer = true; }) end, key = "<leader>b", mode = "n" },
-        { action = "<cmd>Telescope live_grep<cr>", key = "<leader>g", mode = "n" },
+        { action = function () telescope.extensions.live_grep_args.live_grep_args() end, key = "<leader>gg", mode = "n" },
         { action = "<cmd>Telescope jumplist<cr>", key = "<leader>o", mode = "n" },
         { action = "<cmd>Telescope oldfiles<cr>", key = "<leader>p", mode = "n" },
         { action = function() vim.cmd("SessionManager load_session") end, key = "<leader>s", mode = "n", options = { noremap = true, silent = true } },
         { action = "<Esc>", key = "<A-i>", mode = {"n", "i", "v", "c", "o"}, options = { noremap = true, silent = true } },
         { action = "<NOP>", key = " ", mode = "n" },
         -- { action = ":Telescope file_browser path=%:p:h select_buffer=true<CR>", key = "<C-e>", mode = "n", options = { silent = true; } },
-        { action = function() require("telescope").extensions.file_browser.file_browser(vim.tbl_deep_extend('force', telescope_fb_opts, {path="%:p:h"; select_buffer=true;})) end, key = "<C-e>", mode = "n", options = { silent = true; } },
-        { action = function() require("telescope").extensions.file_browser.file_browser(telescope_fb_opts) end, key = "<C-M-e>", mode = "n", options = { silent = true; } },
+        { action = function() telescope.extensions.file_browser.file_browser(vim.tbl_deep_extend('force', telescope_fb_opts, {path="%:p:h"; select_buffer=true;})) end, key = "<C-e>", mode = "n", options = { silent = true; } },
+        { action = function() telescope.extensions.file_browser.file_browser(telescope_fb_opts) end, key = "<C-M-e>", mode = "n", options = { silent = true; } },
         { action = ":Telescope project<CR>", key = "<leader>fp", mode = "n" },
         { action = function() vim.lsp.buf.hover() end, key = "<C-p>", mode = {"n", "i"}, options = { remap = true } },
         { action = function() require("dapui").toggle() end, key = "<leader>=", mode = "n", options = { noremap = true } },
@@ -339,3 +358,5 @@ do
 end
 
 vim.diagnostic.config({ signs = false })
+
+TouchTelescopeHistoryDB()
